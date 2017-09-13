4dee12f3b021c94d8387916887a2d7bc30bbdfcf
Ian Harvey <ith.harvey@gmail.com>
2017-09-13 16:36:13 -0500

    all setup now adding ui-router

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './components/CommentBox';
 
-
+ // url='https://profile-crud-app.herokuapp.com/api/comments'
 ReactDOM.render(
  <CommentBox
- url='https://profile-crud-app.herokuapp.com/api/comments'
+ url='http://localhost:5000/api/comments'
  pollInterval={2000} />,
  document.getElementById('root')
 );
